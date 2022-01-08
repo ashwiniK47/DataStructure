4fc7f49b4913b01574a51901160be56e28e6bfbc
ashwiniK47 <ashwinikolekar47@gmail.com>
2022-01-08 22:54:04 +0530

    lsearch,bsearch,rsearch,get,set,max,min,sum,rsum,avg

--- Array ADT/array_adt.cpp
+++ Array ADT/array_adt.cpp
@@ -1,13 +1,11 @@
 #include<iostream>
 #include <algorithm>
-
-
 using namespace std;
 
 struct Array{
     int A[20];
     int size{20};
-    int length;
+    int length{0};
 };
 
 //DISPLAY
@@ -51,8 +49,8 @@ int lsearch(struct Array *a, int x){
     for(int i{0}; i < a->length; i++){
         if(a->A[i] == x){
             //swap(a->A[i], a->A[0]);        //improving method1-MOVE TO HEAD(direct to index0) 
-            if(i > 0)                       //improving method2-TRANSPOSITION(one index closer)
-            swap(a->A[i], a->A[i-1]);
+            // if(i > 0)                       //improving method2-TRANSPOSITION(one index closer)
+            // swap(a->A[i], a->A[i-1]);
             return i;
         }
     }
@@ -64,7 +62,7 @@ int bsearch(struct Array a, int x){
     int h{a.length-1};
     int mid{(l+h)/2}; 
 
-    while(l < h){
+    while(l <= h){
         mid = (l+h)/2;
         if(x == a.A[mid])
             return mid;
@@ -75,6 +73,71 @@ int bsearch(struct Array a, int x){
         }
     }
     return -1;
+}
+//RECURSIVE BINARY SEARCH
+int rsearch(int a[], int l, int h, int x){
+    int mid;
+    if(l<=h){
+        mid = (l+h)/2;
+        if(x == a[mid])
+            return mid;
+        else if(x < a[mid])
+            return rsearch(a, l, mid-1, x);
+        else
+            return rsearch(a, mid+1, h , x);
+    }
+    return -1;
+}
+//GET ELEMENT AT INDEX
+int get(struct Array a, int index){
+    if(index>=0 && index<a.length){
+        return a.A[index];
+    }
+    return -1;
+}
+//SET ELEMENT AT INDEX
+void set(struct Array a, int index, int val){
+    if(index>=0 && index<a.length){
+        a.A[index] = val;
+    }
+}
+//MAXIMUM ELEMENT
+int max(struct Array a){
+    int max{a.A[0]};
+    for(int i{1}; i < a.length; i++){
+        if(a.A[i] > max){
+            max = a.A[i];
+        }
+    }
+    return max;
+}
+//MINUMUM ELEMENT
+int min(struct Array a){
+    int min{a.A[0]};
+    for(int i{1}; i < a.length; i++){
+        if(a.A[i] < min){
+            min = a.A[i];
+        }
+    }
+    return min;
+}
+//SUM OF ALL ELEMENTS
+int sum(struct Array a){
+    int total{0};
+    for(int i{0}; i<a.length; i++)
+        total += a.A[i];
+    return total;
+}
+//SUM BY RECURSION 
+int recursiveSum(int a[], int n){
+    if(n <= 0)
+        return 0;
+    else 
+        return recursiveSum(a,n-1) + a[n-1];
+}
+//AVERAGE
+int average(struct Array a){
+    return sum(a)/(a.length-1);
 }
 int main(){
     int n;
@@ -91,14 +154,23 @@ int main(){
 
     //TEST CASES
     display(arr);
-    add(&arr, 8);
+    add(&arr, 100);
     display(arr);
     insert(&arr, 2, 0);
     display(arr);
     del(&arr, 2);
     display(arr);
-    cout << "Searched element at index: " << lsearch(&arr, 8) << endl;
-    display(arr);
+    cout << "LinearSearched element at index: " << lsearch(&arr, 100) << endl;
+    cout << "BinarySearched element at index: " << bsearch(arr, 8) << endl;
+    cout << "BinarySearched element at index: " << bsearch(arr, 7) << endl;
+    cout << "RecursiveBinarSearched element at index: " << rsearch(arr.A, 0, arr.length-1, 7) << endl;
+    cout << "Maximum element from array: " << max(arr) << endl;
+    cout << "Maximum element from array: " << min(arr) << endl;
+    cout << "Sum of all elements in array: " << sum(arr) << endl;
+    cout << "Sum of all elements in array: " << recursiveSum(arr.A, arr.length) << endl;
+    cout << "Average of all elements in array: " << average(arr) << endl;
+
+
 
     return 0;
 }
